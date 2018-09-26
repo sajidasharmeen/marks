fb22e9f3f7f9537098d8ffafb593870c6c2e4f45
sajidasharmeen <42132932+sajidasharmeen@users.noreply.github.com>
2018-09-26 12:04:18 +0500

    .net project
    miss tehreem

---
+++ ImageStore_Example/ImageStore_Example/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ImageStore_Example.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Select any image"></asp:Label>
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br /><br />
+        <asp:Button ID="Button1" runat="server" Text="save image" OnClick="Button1_Click" />
+        <br />
+        <br />
+       <h2 style="color: #FF0000">show image</h2>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button2" runat="server" Text="show"  OnClick="Button2_Click"/>
+        <br />
+        <br />
+        <asp:Image ID="Image1" runat="server" Width="200" Height="200" />
+        <br />
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ ImageStore_Example/ImageStore_Example/WebForm1.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ImageStore_Example
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        String conn = ConfigurationManager.ConnectionStrings["Database1"].ConnectionString.ToString();
+        string imgname;
+        int imgsize;
+        string imgpath;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            imgname = FileUpload1.FileName.ToString();
+            imgpath = "images/" + imgname;
+            FileUpload1.SaveAs(Server.MapPath(imgpath));
+            imgsize = FileUpload1.PostedFile.ContentLength;
+
+            if (FileUpload1.PostedFile != null && FileUpload1.PostedFile.FileName != "")
+            {
+                if (FileUpload1.PostedFile.ContentLength > 51200) //50 mb
+                {
+                    Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "Alert", "alert('file is too big')", true);
+                }
+                else
+                {
+                    try
+                        {
+                        SqlConnection cn=new SqlConnection(conn);
+                        cn.Open();
+                      
+                       string sql_query="insert into img_table (img_name,img_size,img_path) values ('"+imgname+"','"+imgsize+"','"+imgpath+"')";
+                        SqlCommand cmd=new SqlCommand(sql_query,cn);
+                        cmd.ExecuteNonQuery();
+                        Response.Write("image saved in database");
+                        cn.Close();
+                          }
+                      catch (Exception ex)
+                      {
+                         Response.Write(ex.Message);
+                     }
+        }
+
+                }
+            }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+         //show image
+            try
+            {
+                SqlConnection cn = new SqlConnection(conn);
+                cn.Open();
+
+                string sql_query = "select * from img_table where img_name='"+TextBox1.Text+"' ";
+                SqlCommand cmd = new SqlCommand(sql_query, cn);
+                cmd.ExecuteNonQuery();
+              Image1.ImageUrl = "images/"+ TextBox1.Text;
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+
+        }
+        }
+
+       
+            
+
+       
+    }
